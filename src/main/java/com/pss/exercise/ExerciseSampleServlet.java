ec4aafe3fd08707b62cc8724c76462f24a25bda2
lildgman <humanorc2357@gmail.com>
2024-04-03 21:13:33 +0900

    dd

--- src/main/java/com/pss/exercise/ExerciseSampleServlet.java
+++ src/main/java/com/pss/exercise/ExerciseSampleServlet.java
@@ -10,7 +10,7 @@ import javax.servlet.http.HttpServletResponse;
 /**
  * Servlet implementation class BoardSampleServlet
  */
-@WebServlet("/ExerciseSampleServlet")
+@WebServlet("/ExerciseSampleServlett")
 public class ExerciseSampleServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
