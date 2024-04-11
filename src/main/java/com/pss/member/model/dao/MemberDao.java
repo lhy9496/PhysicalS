6dfb69c6422c2a49bff718dc988be8ea3ba6b374
lhy9496 <dlghdus1949@gmail.com>
2024-04-11 19:55:43 +0900

    insertMember 개편중

--- src/main/java/com/pss/member/model/dao/MemberDao.java
+++ src/main/java/com/pss/member/model/dao/MemberDao.java
@@ -1,27 +1,11 @@
 package com.pss.member.model.dao;
 
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
-import java.util.Properties;
+
 
 import com.pss.member.model.vo.Member;
-import static com.pss.common.JDBCTemplate.*;
+
 
 public class MemberDao {
-	private Properties prop = new Properties();
-	
-	public MemberDao() {
-		String filePath = MemberDao.class.getResource("db/sql/member-mapper.xml").getPath();
-		
-		try {
-			prop.loadFromXML(new FileInputStream(filePath));
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-	}
 	
 	public int insertMember(Connection conn, Member m) {
 		PreparedStatement pstmt = null;
--- src/main/java/com/pss/member/service/MemberService.java
+++ src/main/java/com/pss/member/service/MemberService.java
@@ -1,24 +1,8 @@
 package com.pss.member.service;
 
-import java.sql.Connection;
-import static com.pss.common.JDBCTemplate.*;
-
-import com.pss.member.model.dao.MemberDao;
 import com.pss.member.model.vo.Member;
 
-public class MemberService {
-	public int insertMember(Member m) {
-		Connection conn = getConnection();
-		
-		int result = new MemberDao().insertMember(conn, m);
-		
-		if(result > 0) {
-			commit(conn);
-		} else {
-			rollback(conn);
-		}
-		close(conn);
-		
-		return result;
-	}
+public interface MemberService {
+	public abstract int insertMember(Member m);
+	
 }
